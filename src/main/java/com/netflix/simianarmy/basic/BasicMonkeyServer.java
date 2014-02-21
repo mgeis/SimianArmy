7b26b2691f068a305d41c022fb649052e8d5b43e
Matt Geis <mgeis@salesforce.com>
2014-02-20 17:58:37 -0800

    first submission for review

--- src/main/java/com/netflix/simianarmy/basic/BasicMonkeyServer.java
+++ src/main/java/com/netflix/simianarmy/basic/BasicMonkeyServer.java
@@ -24,16 +24,10 @@ import java.util.Properties;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
-import com.netflix.simianarmy.basic.conformity.BasicConformityMonkey;
-import com.netflix.simianarmy.basic.conformity.BasicConformityMonkeyContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.netflix.simianarmy.MonkeyRunner;
-import com.netflix.simianarmy.aws.janitor.VolumeTaggingMonkey;
-import com.netflix.simianarmy.basic.janitor.BasicJanitorMonkey;
-import com.netflix.simianarmy.basic.janitor.BasicJanitorMonkeyContext;
-import com.netflix.simianarmy.basic.janitor.BasicVolumeTaggingMonkeyContext;
 
 /**
  * Will periodically run the configured monkeys.
@@ -51,16 +45,31 @@ public class BasicMonkeyServer extends HttpServlet {
     public void addMonkeysToRun() {
         LOGGER.info("Adding Chaos Monkey.");
         RUNNER.replaceMonkey(this.chaosClass, this.chaosContextClass);
-        LOGGER.info("Adding Volume Tagging Monkey.");
-        RUNNER.replaceMonkey(VolumeTaggingMonkey.class, BasicVolumeTaggingMonkeyContext.class);
-        LOGGER.info("Adding Janitor Monkey.");
-        RUNNER.replaceMonkey(BasicJanitorMonkey.class, BasicJanitorMonkeyContext.class);
-        LOGGER.info("Adding Conformity Monkey.");
-        RUNNER.replaceMonkey(BasicConformityMonkey.class, BasicConformityMonkeyContext.class);
+
+        if (this.volumeTaggingClass != null) {
+            LOGGER.info("Adding Volume Tagging Monkey.");
+            RUNNER.replaceMonkey(this.volumeTaggingClass, this.volumeTaggingContextClass);
+        } else {
+            LOGGER.info("Skipping Volume Tagging Monkey.");
+        }
+
+        if (this.conformityClass != null) {
+            LOGGER.info("Adding Conformity Monkey.");
+            RUNNER.replaceMonkey(this.conformityClass, this.conformityContextClass);
+        } else {
+            LOGGER.info("Skipping Conformity Monkey.");
+        }
+
+        if (this.janitorClass != null) {
+            LOGGER.info("Adding Janitor Monkey.");
+            RUNNER.replaceMonkey(this.janitorClass, this.janitorContextClass);
+        } else {
+            LOGGER.info("Skipping Janitor Monkey.");
+        }
     }
 
     /**
-     * make the class of the client object configurable.
+     * make the class of the chaos context object configurable.
      */
     @SuppressWarnings("rawtypes")
     private Class chaosContextClass = com.netflix.simianarmy.basic.BasicChaosMonkeyContext.class;
@@ -70,6 +79,48 @@ public class BasicMonkeyServer extends HttpServlet {
      */
     @SuppressWarnings("rawtypes")
     private Class chaosClass = com.netflix.simianarmy.basic.chaos.BasicChaosMonkey.class;
+
+    /**
+     * make the class of the conformity context object configurable.
+     */
+    @SuppressWarnings("rawtypes")
+    private Class conformityContextClass = null;
+    // default is com.netflix.simianarmy.basic.conformity.BasicConformityMonkeyContext.class;
+
+    /**
+     * make the class of the conformity object configurable.
+     */
+    @SuppressWarnings("rawtypes")
+    private Class conformityClass = null;
+    // default is com.netflix.simianarmy.basic.conformity.BasicConformityMonkey.class;
+
+    /**
+     * make the class of the janitor context object configurable.
+     */
+    @SuppressWarnings("rawtypes")
+    private Class janitorContextClass = null;
+    // default is com.netflix.simianarmy.basic.janitor.BasicJanitorMonkeyContext.class;
+
+    /**
+     * make the class of the janitor object configurable.
+     */
+    @SuppressWarnings("rawtypes")
+    private Class janitorClass = null;
+    // default is com.netflix.simianarmy.basic.janitor.BasicJanitorMonkey.class;
+
+    /**
+     * make the class of the volume tagging context object configurable.
+     */
+    @SuppressWarnings("rawtypes")
+    private Class volumeTaggingContextClass = null;
+    // default is com.netflix.simianarmy.basic.janitor.BasicVolumeTaggingMonkeyContext.class;
+
+    /**
+     * make the class of the volume tagging object configurable.
+     */
+    @SuppressWarnings("rawtypes")
+    private Class volumeTaggingClass = null;
+    // default is com.netflix.simianarmy.aws.janitor.VolumeTaggingMonkey.class;
 
     @Override
     public void init() throws ServletException {
@@ -81,6 +132,7 @@ public class BasicMonkeyServer extends HttpServlet {
 
     /**
      * Loads the client that is configured.
+     *
      * @throws ServletException
      *             if the configured client cannot be loaded properly
      */
@@ -93,10 +145,19 @@ public class BasicMonkeyServer extends HttpServlet {
 
         Class newChaosClass = loadClientClass(clientConfig, "simianarmy.client.chaos.class");
         this.chaosClass = (newChaosClass == null ? this.chaosClass : newChaosClass);
+
+        this.conformityContextClass = loadClientClass(clientConfig, "simianarmy.client.conformity.context.class");
+        this.conformityClass = loadClientClass(clientConfig, "simianarmy.client.conformity.class");
+
+        this.volumeTaggingContextClass = loadClientClass(clientConfig, "simianarmy.client.volumeTagging.context.class");
+        this.volumeTaggingClass = loadClientClass(clientConfig, "simianarmy.client.volumeTagging.class");
+
+        this.janitorContextClass = loadClientClass(clientConfig, "simianarmy.client.janitor.context.class");
+        this.janitorClass = loadClientClass(clientConfig, "simianarmy.client.janitor.class");
     }
 
     @SuppressWarnings("rawtypes")
-    private Class loadClientClass(Properties clientConfig, String key) throws ServletException {
+    private Class loadClientClass(Properties clientConfig, String key)  throws ServletException {
         ClassLoader classLoader = BasicMonkeyServer.class.getClassLoader();
         try {
             String clientClassName = clientConfig.getProperty(key);
@@ -104,7 +165,7 @@ public class BasicMonkeyServer extends HttpServlet {
                 LOGGER.info("using standard client for " + key);
                 return null;
             }
-        Class newClass = classLoader.loadClass(clientClassName);
+            Class newClass = classLoader.loadClass(clientClassName);
             LOGGER.info("using " + key + " loaded " + newClass.getCanonicalName());
             return newClass;
         } catch (ClassNotFoundException e) {
@@ -148,9 +209,11 @@ public class BasicMonkeyServer extends HttpServlet {
         LOGGER.info("Stopping Chaos Monkey.");
         RUNNER.removeMonkey(this.chaosClass);
         LOGGER.info("Stopping volume tagging Monkey.");
-        RUNNER.removeMonkey(VolumeTaggingMonkey.class);
+        RUNNER.removeMonkey(this.volumeTaggingClass);
         LOGGER.info("Stopping Janitor Monkey.");
-        RUNNER.removeMonkey(BasicJanitorMonkey.class);
+        RUNNER.removeMonkey(this.janitorClass);
+        LOGGER.info("Stopping Conformity Monkey.");
+        RUNNER.removeMonkey(this.conformityClass);
         super.destroy();
     }
 }
